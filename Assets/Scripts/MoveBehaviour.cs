6eca767e5ba151e43a5f4fd2c8a50112036c37a1
Alejandro Martin <alejandro.martin.7e8@itb.cat>
2025-10-24 17:17:05 +0200

    Added MoveBehavour

---
+++ Assets/Scripts/MoveBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MovementBehaviour : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    public float speed;
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+    public void MoveCharacter(Vector2 direction)
+    {
+        _rb.linearVelocity = direction * speed;
+    }
+    public void Jump()
+    {
+
+    }
+}
