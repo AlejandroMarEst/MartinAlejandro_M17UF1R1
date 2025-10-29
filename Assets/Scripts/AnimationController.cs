7dd65fcc872edc5b88ab4934aaa452575dd7b6de
Alejandro Martin <alejandro.martin.7e8@itb.cat>
2025-10-29 19:11:25 +0100

    Tried doing raycast

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void RunAnimation(Vector2 direction)
+    {
+        if (direction.x > 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else if (direction.x < 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        _animator.SetFloat("Velocity", direction.magnitude);
+    }
+}
--- Assets/Scripts/MoveBehaviour.cs
+++ Assets/Scripts/MoveBehaviour.cs
@@ -3,18 +3,49 @@ using UnityEngine;
 public class MoveBehaviour : MonoBehaviour
 {
     private Rigidbody2D _rb;
+    private AnimationController _anim;
     public float speed;
     public float jumpHeight;
+    private bool isGrounded;
+    private RaycastHit2D groundCheck;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<AnimationController>();
+    }
+    void FixedUpdate()
+    {
+        groundCheck = Physics2D.Raycast(_rb.position, Vector2.down, 1f);
+        if (groundCheck.collider != null)
+        {
+            if (groundCheck.collider.name == "Ground")
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+            Debug.Log(groundCheck.collider.name);
+            Debug.DrawRay(_rb.position, Vector2.down, Color.red);
+        } else
+        {
+            isGrounded = false;
+        }
     }
     public void MoveCharacter(Vector2 direction)
     {
         _rb.linearVelocity = new Vector2(direction.normalized.x * speed, _rb.linearVelocity.y);
+        _anim.RunAnimation(direction);
+
+
     }
     public void Jump()
     {
-        _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpHeight);
+        if (true)
+        {
+            _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpHeight);
+        }
     }
 }
