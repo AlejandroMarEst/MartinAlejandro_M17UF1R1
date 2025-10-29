beeed095456c3ec431b01b0646ddad1223c92ce0
Alex <alejandromarest9@gmail.com>
2025-10-29 13:15:23 +0100

    Worked on movement, character can now jump and move while holding

--- Assets/Scripts/MoveBehaviour.cs
+++ Assets/Scripts/MoveBehaviour.cs
@@ -4,6 +4,7 @@ public class MoveBehaviour : MonoBehaviour
 {
     private Rigidbody2D _rb;
     public float speed;
+    public float jumpHeight;
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -14,6 +15,8 @@ public class MoveBehaviour : MonoBehaviour
     }
     public void Jump()
     {
-
+        Vector2 vector = new Vector2();
+        vector.y = jumpHeight;
+        _rb.linearVelocity += vector;
     }
 }
